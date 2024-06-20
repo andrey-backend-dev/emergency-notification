202ab1977d3334cef4151eab149ac38f16495581
Andrey Semenov <tocode.andrey@gmail.com>
2024-06-21 00:12:59 +0300

    caller repository method added

--- src/main/java/org/example/repository/CallerRepository.java
+++ src/main/java/org/example/repository/CallerRepository.java
@@ -4,6 +4,10 @@ import org.example.entity.Caller;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CallerRepository extends CrudRepository<Caller, Long> {
+    Optional<Caller> findByUsername(String username);
+    void deleteByUsername(String username);
 }
